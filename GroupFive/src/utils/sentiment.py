effb1317dfed0e50ec0f94f4989d0599ee85db47
Márton Kardos <power.up1163@gmail.com>
2022-04-30 17:47:06 +0200

    Initial commit for Group 5

---
+++ GroupFive/src/utils/sentiment.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+import numpy as np
+import pandas as pd
+from textblob import TextBlob
+
+import utils.text
+
+
+def sentiment_score(text: str) -> Tuple[float, float]:
+    if not text or not isinstance(text, str):
+        return np.nan, np.nan
+    analysis = TextBlob(text)
+    return analysis.sentiment.polarity, analysis.sentiment.subjectivity
+
+
+def add_sentiment(df: pd.DataFrame, based_on: str) -> pd.DataFrame:
+    polarity, subjectivity = zip(*[sentiment_score(text) for text in df[based_on]])
+    return df.assign(sentiment_polarity=polarity, sentiment_subjectivity=subjectivity)
